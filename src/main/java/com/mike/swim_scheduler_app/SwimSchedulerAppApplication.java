c62e7d6f9225a85ced3f8915f622cc5d79a43768
mihai22z <mihail.zamfirescu@yahoo.com>
2024-07-20 00:18:34 +0300

    Initial commit

---
+++ src/main/java/com/mike/swim_scheduler_app/SwimSchedulerAppApplication.java
@@ -0,0 +1,13 @@
+package com.mike.swim_scheduler_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimSchedulerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimSchedulerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mike/swim_scheduler_app/SwimSchedulerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.swim_scheduler_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimSchedulerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
